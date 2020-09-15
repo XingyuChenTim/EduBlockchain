2c7fc64becb671f69b389a6ef7b57d69743874e6
XingyuChenTim <46166892+XingyuChenTim@users.noreply.github.com>
2020-09-15 13:26:51 -0500

    login function set up
    login function set up

--- project/src/main/java/com/example/project/controller/UserController.java
+++ project/src/main/java/com/example/project/controller/UserController.java
@@ -3,13 +3,11 @@ package com.example.project.controller;
 import com.example.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/capstone")
 public class UserController {
 
     @Autowired
--- project/src/main/java/com/example/project/controller/webController.java
+++ project/src/main/java/com/example/project/controller/webController.java
@@ -1,20 +1,32 @@
 package com.example.project.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import com.example.project.form.LoginForm;
 import com.example.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/capstone")
 public class WebController {
 
     @Autowired
     UserRepository userRepository;
 
+    @GetMapping("/")
+    public String home1(){
+        return "home";
+    }
+
     @GetMapping("/home")
-    public String home(){
+    public String home2(){
         return "home";
     }
 
@@ -33,5 +45,24 @@ public class WebController {
         return "user";
     }
 
-    
+    @PostMapping(value="/login")
+    public String login(@ModelAttribute(name="loginForm") LoginForm loginForm, Model model) {
+        String username = loginForm.getUsername();
+        String password = loginForm.getPassword();
+        if("test".equals(username) && "test".equals(password)) {
+            return "user";
+        }
+        model.addAttribute("invalidCredentials", true);
+        return "login";
+    }
+    /*
+    @PostMapping(value="/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String, Object> map, HttpSession session) {
+        if("admin".equals(username) && "admin".equals(password)) {
+            session.setAttribute("loginuser", username);
+            return "user";
+        }
+        return "user";
+    }
+    */
 }
