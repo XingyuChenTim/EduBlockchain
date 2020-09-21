fc8ffcab89f7f20721591fec5cd5d3582a42f36e
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-09-21 18:22:42 -0500

    Test class of UserForm

---
+++ project/src/main/java/com/example/project/controller/WebController.java
@@ -0,0 +1,46 @@
+package com.example.project.controller;
+
+import com.example.project.form.UserForm;
+import com.example.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+// Will try to use RestControlller in the future implemetation
+@Controller
+public class WebController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/sign")
+    public String login() {
+        return "sign";
+    }
+
+    @PostMapping(value = "/sign")
+    public String login(UserForm sig) {
+        if (userRepository.signinUser(sig)) {
+            return "user";
+        }
+        return "sign";
+    }
+
+    @PostMapping(value = "/register")
+    public String userInfo(UserForm reg) {
+        userRepository.registerUser(reg);
+        return "home";
+    }
+
+}
---
+++ project/src/test/java/com/example/project/form/UserFormTest.java
@@ -0,0 +1,33 @@
+package com.example.project.form;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserFormTest {
+
+	private UserForm u = new UserForm();
+	@Test
+	public void testConstructorOne() {
+		u.setPassword("123456");
+		assertEquals(u.getPassword(), "123456");
+	}
+	
+	@Test
+	public void testSetWallet() {
+		u.setWallet(123.2);
+		assertEquals(u.getWallet(), 123.2);
+	}
+	
+	@Test
+	public void testSetUsername() {
+		u.setUsername("Hello");
+		assertEquals(u.getUsername(), "Hello");
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals(u.toString(), "UserForm [password=" + u.getPassword() + ", username=" 
+	+ u.getUsername() + ", wallet=" + u.getWallet() + "]");
+	}
+}
