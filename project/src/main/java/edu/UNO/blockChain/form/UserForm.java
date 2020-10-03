937e6793eaced71e5399cfed941d09697fc7760b
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-10-03 17:06:57 -0500

    Documentation
    For all files under src/main/java

--- project/src/main/java/edu/UNO/blockChain/form/UserForm.java
+++ project/src/main/java/edu/UNO/blockChain/form/UserForm.java
@@ -2,8 +2,14 @@ package edu.UNO.blockChain.form;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+/*
+ * The class of user object.
+ */
 @EntityScan
 public class UserForm {
+	/*
+	 * Variables of username, password and walletBalance.
+	 */
     private String username;
     private String password;
     private Double wallet;
@@ -11,27 +17,39 @@ public class UserForm {
     public UserForm() {
         super();
     }
-
+    /*
+     * Get method for user name.
+     */
     public String getUsername() {
         return username;
     }
-
+    /*
+     * Set method for user name.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    /*
+     * Get method for password.
+     */
     public String getPassword() {
         return password;
     }
-
+    /*
+     * Set method for password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /*
+     * Get method for wallet.
+     */
     public Double getWallet() {
         return wallet;
     }
-
+    /*
+     * Set method for wallet.
+     */
     public void setWallet(Double wallet) {
         this.wallet = wallet;
     }
