2b6aa9c063debfaa1ff7c6f7e67f69f51408cee6
XingyuChenTim <46166892+XingyuChenTim@users.noreply.github.com>
2020-11-05 13:17:26 -0600

    minereward function update

--- project/src/main/java/edu/uno/blockchain/repository/Broadcastrepo.java
+++ project/src/main/java/edu/uno/blockchain/repository/Broadcastrepo.java
@@ -3,8 +3,13 @@ package edu.uno.blockchain.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
+
+import static edu.uno.blockchain.function.Hashfunctions.sha256hash;
+
 import edu.uno.blockchain.form.Broadcastform;
 import edu.uno.blockchain.form.Tokenform;
 import edu.uno.blockchain.form.TokenMapper;
@@ -38,5 +43,19 @@ public class Broadcastrepo {
 
         return jdbcTemplate.query("SELECT * FROM transactionpoll", new PollMapper());
 
+    }
+
+    public String minereward(String user, String userprivatekey) {
+        String sql = "insert into token(token, owner) values(?,?)";
+        String tokenkey = "";
+        for (int i = 0; i < 5; i++) {
+            try {
+                tokenkey = sha256hash(userprivatekey + i);
+                jdbcTemplate.update(sql, tokenkey, user);
+            } catch (NoSuchAlgorithmException e) {
+                return "reward insert failed";
+            }
+        }
+        return "reward inserted";
     }
 }
