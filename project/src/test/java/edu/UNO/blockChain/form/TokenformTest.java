ef7445455b43efcecb2975d86e5834e1c91ca00f
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-11-16 21:34:51 -0600

    Update TokenformTest.java

--- project/src/test/java/edu/UNO/blockChain/form/TokenformTest.java
+++ project/src/test/java/edu/UNO/blockChain/form/TokenformTest.java
@@ -5,10 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TokenformTest {
-
+	
+	private Tokenform t = new Tokenform();
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Tokenform tf = new Tokenform(256, "gg", "mm");
+		assertEquals(tf.getId(), 256);
+		assertEquals(tf.getToken(), "gg");
+		assertEquals(tf.getOwner(), "mm");
 	}
-
+	
+	@Test
+	public void testSetId() {
+		t.setId(8899);
+		assertEquals(t.getId(), 8899);
+	}
+	
+	@Test
+	public void testSetToken() {
+		t.setToken("hola");
+		assertEquals(t.getToken(), "hola");
+	}
+	
+	@Test
+	public void testSetOwner() {
+		t.setOwner("bye");
+		assertEquals(t.getOwner(), "bye");
+	}
 }
