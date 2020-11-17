7de01487351ea85cb295e8463d1bdcb24fb473f3
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-11-16 21:08:08 -0600

    Update BroadcastformTest.java

--- project/src/test/java/edu/UNO/blockChain/form/BroadcastformTest.java
+++ project/src/test/java/edu/UNO/blockChain/form/BroadcastformTest.java
@@ -5,10 +5,33 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class BroadcastformTest {
-
+	
+	
+	private Broadcastform b = new Broadcastform();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Broadcastform bc = new Broadcastform("hh", 12, "ii");
+		assertEquals(bc.getAmount(), 12);
+		assertEquals(bc.getHashid(), "ii");
+		assertEquals(bc.getReceiver(), "hh");
 	}
-
+	
+	@Test
+	public void testSetAmount() {
+		b.setAmount(122);
+		assertEquals(b.getAmount(), 122);
+	}
+	
+	@Test
+	public void testSetHashid() {
+		b.setHashid("aaa");
+		assertEquals(b.getHashid(), "aaa");
+	}
+	
+	@Test
+	public void testSetReceiver() {
+		b.setReceiver("bbb");
+		assertEquals(b.getReceiver(), "bbb");
+	}
 }
