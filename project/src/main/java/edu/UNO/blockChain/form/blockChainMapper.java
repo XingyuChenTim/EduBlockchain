8c3d6f20ecba31c3360daf4b3a8e967145a68ed0
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-11-30 15:46:13 -0600

    Update blockChainMapper.java

--- project/src/main/java/edu/UNO/blockChain/form/blockChainMapper.java
+++ project/src/main/java/edu/UNO/blockChain/form/blockChainMapper.java
@@ -4,8 +4,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
-
+/**
+ * The class is the block chain mapper.
+ * @author fire-holder
+ *
+ */
 public class blockChainMapper implements RowMapper<blockChainform>{
+	/**
+	 * To map a row of block chain.
+	 * @param rs The result set.
+	 * @param rowNum The number of rows.
+	 * @return The block chain that have set each data value.
+	 * @throws SQLException
+	 */
     @Override
 	public blockChainform mapRow(ResultSet rs, int rowNum) throws SQLException {
         
